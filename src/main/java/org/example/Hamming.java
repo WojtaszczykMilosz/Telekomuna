f714f6c54bcddea8c858b4631f90bf8b74fd9b86
Miłosz Wojtaszczyk <242567@edu.p.lodz.pl>
2023-04-01 16:02:25 +0200

    zmiana

--- src/main/java/org/example/Hamming.java
+++ src/main/java/org/example/Hamming.java
@@ -1,34 +1,84 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Hamming {
 
-    private static int[][] H2 = {
-            {1,1,0,1,1,0,1,0, 1,0,0,0},
-            {1,0,1,1,0,1,1,0, 0,1,0,0},
-            {0,1,1,1,0,0,0,1, 0,0,1,0},
-            {0,0,0,0,1,1,1,1, 0,0,0,1}
-    };
+//    private static final int[][] H2 = {
+//            {1,1,0,1,1,0,1,0, 1,0,0,0},
+//            {1,0,1,1,0,1,1,0, 0,1,0,0},
+//            {0,1,1,1,0,0,0,1, 0,0,1,0},
+//            {0,0,0,0,1,1,1,1, 0,0,0,1}
+//    };
 
-    private static int[][] H = {
-    {0,1,1,1,1,1,1,1, 1,0,0,0,0,0,0,0},
-    {1,0,1,1,1,1,1,1, 0,1,0,0,0,0,0,0},
-    {1,1,0,1,1,1,1,1, 0,0,1,0,0,0,0,0},
-    {1,1,1,0,1,1,1,1, 0,0,0,1,0,0,0,0},
-    {1,1,1,1,0,1,1,1, 0,0,0,0,1,0,0,0},
-    {1,1,1,1,1,0,1,1, 0,0,0,0,0,1,0,0},
-    {1,1,1,1,1,1,0,1, 0,0,0,0,0,0,1,0},
-    {1,1,1,1,1,1,1,0, 0,0,0,0,0,0,0,1},
-    };
+    private static final int[][] H = {
+            {1, 1, 1, 1, 0, 0, 0, 0,     1, 0, 0, 0, 0, 0, 0, 0},
+            {1, 1, 0, 0, 1, 1, 0, 0,     0, 1, 1, 0, 0, 0, 0, 0},
+            {1, 0, 1, 0, 1, 0, 1, 0,     0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1, 1, 0,     0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 1, 1, 0, 1, 0, 0, 1,     0, 0, 0, 0, 1, 0, 0, 0},
+            {1, 0, 0, 1, 0, 1, 0, 1,     0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 1, 1, 1, 1, 0, 1, 1,     0, 0, 0, 0, 0, 0, 1, 0},
+            {1, 1, 1, 0, 0, 1, 1, 1,     0, 0, 0, 0, 0, 0, 0, 1}};
+    public static void sprawdzH(){
+        for (int i =0;i<H[0].length;i++){
+            for (int j =0;j<H[0].length;j++){
+                if(i != j){
+                    if(Arrays.compare(getColumn(i),getColumn(j))==0){
+                        System.out.println("Błąd");
+                        return;
+                    }
+                }
+            }
+        }
 
+
+        for (int i =0;i<H[0].length;i++){
+            for (int j =0;j<H[0].length;j++){
+                if(i != j) {
+                    int[] tab = XOR(getColumn(i), getColumn(j));
+                    for(int y =0;y<H[0].length;y++){
+                        if(Arrays.compare(getColumn(y),tab)==0){
+                            System.out.println("Błąd");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        for (int i =0;i<H[0].length;i++){
+            for (int j =0;j<H[0].length;j++){
+                if(i != j) {
+                    int[] tab = XOR(getColumn(i), getColumn(j));
+                    for (int y =0;y<H[0].length;y++){
+                        for (int x =0;x<H[0].length;x++){
+                            if(x!= y) {
+                                int[] tab2 = XOR(getColumn(x), getColumn(y));
+                                if((x != i && y != j) && (x != j && y != i)){
+                                    if(Arrays.compare(tab,tab2) == 0){
+                                        System.out.println("Błąd");
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("Brak błędu");
+    }
     public static int[] sprawdz(int[] wiadomosc){
         int x;
-
         int[] E= new int[H.length];
-        if (wiadomosc.length != H[0].length) return null;
+
+        if (wiadomosc.length != H[0].length) {
+            return null;
+        }
+
         for (int i = 0; i< H.length ; i++) {
             x=0;
             for (int j = 0; j<wiadomosc.length; j++){
@@ -62,11 +112,14 @@ public class Hamming {
 
     public static int[] napraw(int[] wiadomosc) {
         int[] E = sprawdz(wiadomosc);
-        if (E == null) return wiadomosc;
-        int[] wyj = wiadomosc;
+        if (E == null) {
+            return wiadomosc;
+        }
+        int[] wyj = Arrays.copyOf(wiadomosc,H.length);
         int[] col;
         for (int i = 0; i < H.length; i++) {
             col = getColumn(i);
+
             if (Arrays.compare(col,E) == 0) {
                 wyj[i] = wiadomosc[i] ^ 1;
             }
@@ -77,9 +130,12 @@ public class Hamming {
 
     public static int[] napraw2(int[] wiadomosc) {
         int[] E = sprawdz(wiadomosc);
-        if (E == null) return wiadomosc;
+        if (E == null) {
+            return wiadomosc;
+        }
 
-        int[] wyj = Arrays.copyOf(wiadomosc,wiadomosc.length);
+        int[] wyj = Arrays.copyOf(wiadomosc,H.length);
+        int[] wej = Arrays.copyOf(wiadomosc,H.length);
         int[] col;
         for (int i = 0; i < H.length; i++) {
             col = getColumn(i);
@@ -87,18 +143,24 @@ public class Hamming {
                 wyj[i] = wiadomosc[i] ^ 1;
             }
         }
-        if (Arrays.compare(wyj,wiadomosc) != 0)
+        if (Arrays.compare(wyj,wej) != 0) {
             return wyj;
+        }
+
         int[] tab;
 
         for (int i = 0; i < H.length; i++) {
             col = getColumn(i);
 
-            for (int j = i+1; j < H.length; j++){
+            for (int j = i+1; j < H[0].length; j++){
                 tab = XOR(col,getColumn(j));
                 if (Arrays.compare(tab,E) == 0) {
+                    System.out.println(i);
+                    System.out.println(j);
                     wyj[i] = wiadomosc[i] ^ 1;
-                    wyj[j] = wiadomosc[j] ^ 1;
+                    if (j < wyj.length) {
+                        wyj[j] = wiadomosc[j] ^ 1;
+                    }
                 }
             }
         }
@@ -110,10 +172,10 @@ public class Hamming {
         int rozmiar = (wiadomosc.length - 1)/8 + 1;
         int[] wyj;
         if (wiadomosc.length % 8 == 0) {
-            wyj = new int[rozmiar*8 + rozmiar*H2.length];
+            wyj = new int[rozmiar*8 + rozmiar*H.length];
         } else{
-            wyj = new int[rozmiar*8 + rozmiar*H2.length + 1];
-            wyj[rozmiar*8 + rozmiar*H2.length]  = wiadomosc.length%8;
+            wyj = new int[rozmiar*8 + rozmiar*H.length + 1];
+            wyj[rozmiar*8 + rozmiar*H.length]  = wiadomosc.length%8;
         }
 
 
@@ -124,13 +186,13 @@ public class Hamming {
             buffor = Arrays.copyOfRange(wiadomosc,i*8,(i+1)*8);
             for (int j = 0; j < buffor.length; j++)
                 wyj[x + j] = buffor[j];
-            for (int j = 0; j < H2.length; j++) {
+            for (int j = 0; j < H.length; j++) {
                 suma = 0;
                 for (int k = 0 ;k < 8;k++)
-                    suma += buffor[k] * H2[j][k];
+                    suma += buffor[k] * H[j][k];
                 wyj[x+buffor.length + j] = suma%2;
             }
-            x+=8+H2.length;
+            x+=8+H.length;
 
 
         }
@@ -142,9 +204,9 @@ public class Hamming {
 
     public static int[] dekoduj(int[] wiadomosc){
 
-        int rozmiar = (wiadomosc.length - 1)/(8+H2.length) + 1;
+        int rozmiar = (wiadomosc.length - 1)/(8+H.length) + 1;
         int[] wyj;
-        boolean nierownosc = wiadomosc.length % (8+H2.length) != 0;
+        boolean nierownosc = wiadomosc.length % (8+H.length) != 0;
         if(nierownosc){
             rozmiar -= 1;
             wyj = new int[rozmiar*8 - wiadomosc[wiadomosc.length - 1]];
@@ -156,10 +218,10 @@ public class Hamming {
         int[] buffor;
         int x = 0;
         for (int i = 0; i < rozmiar; i++){
-            buffor = Arrays.copyOfRange(wiadomosc,i*(8+H2.length),(i+1)*(8+H2.length));
+            buffor = Arrays.copyOfRange(wiadomosc,i*(8+H.length),(i+1)*(8+H.length));
             buffor = napraw(buffor);
             if (!(i == rozmiar - 1 && nierownosc)) {
-                for (int j = 0; j < buffor.length-H2.length; j++)
+                for (int j = 0; j < buffor.length-H.length; j++)
                     wyj[x + j] = buffor[j];
             } else {
                 for (int j = 0; j < wiadomosc[wiadomosc.length-1]; j++)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,18 +8,24 @@ public class Main {
 
     
     public static void main(String[] args) {
-        int[] tab = {1,1,0,1,0,0,1,1, 0,1,1,0};
-        int[] tab2 = {1,0,1,1,0,0,1,0, 1,1,1,1,0,0,1,1};
+        int[] tab = { 1,0,1,1,0,0,1,0, 0,1,1,0};
+        int[] wej= { 1,0,1,1,0,0,1,0};
+        int[] tab2 = {1,0,1,1,0,0,1,0, 1,1,1,0,0,0,1,1};
         int[] wynik = Hamming.sprawdz(tab2);
         int[] nowe = Hamming.napraw2(tab2);
-        int[] wynik2 = Hamming.sprawdz(nowe);
+        int[] wynik2 = Hamming.koduj(wej);
 
 
 
-        for (int i = 0; i<tab.length;i++)
-            System.out.print(tab[i]);
 
-        System.out.println();
+//        for(int i =0;i<nowe.length;i++){
+//            System.out.print(nowe[i]);
+//        }
+
+//        for (int i = 0; i<tab.length;i++)
+//            System.out.print(tab[i]);
+//
+//        System.out.println();
 
 
 //
@@ -36,32 +42,32 @@ public class Main {
 //            System.out.print(wynik2[i]);
 //        System.out.println();
 //
-        int[] plik = OperacjePlikowe.wczytajZpliku("doc.doc");
-        int[] tab3 = Hamming.koduj(plik);
-        int[] tab4 = Hamming.dekoduj(tab3);
-        OperacjePlikowe.zapiszDoPliku("test.doc",tab4);
-        System.out.println();
-//        for (int i = 0; i<plik.length;i++)
-//            System.out.print(plik[i]);
-        System.out.println();
-
-        System.out.println(
-
-        );
-        System.out.println();
-
-
-
-        System.out.println(plik.length%8);
-        System.out.println(plik.length);
-        System.out.println(tab4.length);
-//        tab4 = Hamming.dekoduj(tab3);
-//        for (int i = 0; i<tab4.length;i++)
-//            System.out.print(tab4[i]);
-
-        System.out.println(
-
-        );
+//        int[] plik = OperacjePlikowe.wczytajZpliku("doc.doc");
+//        int[] tab3 = Hamming.koduj(plik);
+//        int[] tab4 = Hamming.dekoduj(tab3);
+//        OperacjePlikowe.zapiszDoPliku("test.doc",tab4);
+//        System.out.println();
+////        for (int i = 0; i<plik.length;i++)
+////            System.out.print(plik[i]);
+//        System.out.println();
+//
+//        System.out.println(
+//
+//        );
+//        System.out.println();
+//
+//
+//
+//        System.out.println(plik.length%8);
+//        System.out.println(plik.length);
+//        System.out.println(tab4.length);
+////        tab4 = Hamming.dekoduj(tab3);
+////        for (int i = 0; i<tab4.length;i++)
+////            System.out.print(tab4[i]);
+//
+//        System.out.println(
+//
+//        );
 
 //        System.out.println(new BigInteger(plik).toString(2));
 
